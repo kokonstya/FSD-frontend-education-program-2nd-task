c446a86318533d6da79898fb8b873d6139bcbe85
kokonstya <kokonstya@gmail.com>
2020-04-13 02:20:29 +0700

    another refactoring

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 // import './components/select-js/select-js'
 // import './components/counter/counter'
 import './components/dropdown/dropdown'
-// import './components/checkbox-list/checkbox-list'
+import './components/checkbox-list/checkbox-list'
 
--- src/components/checkbox-list/checkbox-list.js
+++ src/components/checkbox-list/checkbox-list.js
@@ -1,5 +1,8 @@
 var checkboxList__list = document.querySelector('.checkbox-list__list');
+var checkboxList__label = document.querySelector('.checkbox-list__label');
 var checkboxList__expanded = document.querySelector('#checkbox-list__expanded');
-checkboxList__expanded.addEventListener('click', ()=> {
+checkboxList__label.addEventListener('click', (e)=> {
     checkboxList__list.classList.toggle('active')
+    e.preventDefault()
+    checkboxList__expanded.checked =!checkboxList__expanded.checked
 })
